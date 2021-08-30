bb01bd51794f88b08ca5eb133be8f7afaa01405c
Walter A Rodriguez (UK) <contact@vaskit.com>
2021-08-30 19:14:32 +0100

    Nebular added to the workspace

--- projects/frontend/src/app/app.module.ts
+++ projects/frontend/src/app/app.module.ts
@@ -10,6 +10,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { environment } from 'projects/admin/src/environments/environment';
+import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
 /**
  * Rest of the implementations
  */
@@ -23,7 +25,10 @@ import { environment } from 'projects/admin/src/environments/environment';
     AppRoutingModule,
     BrowserAnimationsModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbLayoutModule,
+    NbEvaIconsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
